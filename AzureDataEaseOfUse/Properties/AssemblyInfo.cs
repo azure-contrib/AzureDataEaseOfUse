de9b782cc2982dae6140c722e15bc93fad94901d
Randy Skopecek <rskopecek@gmail.com>
2014-02-06 22:01:43 -0500

    Added nuspec and updated assembly info

--- AzureDataEaseOfUse/Properties/AssemblyInfo.cs
+++ AzureDataEaseOfUse/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AzureDataEaseOfUse")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Less code for Azure Storage and Caching")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AzureDataEaseOfUse")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("2014 Randy Skopecek")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
