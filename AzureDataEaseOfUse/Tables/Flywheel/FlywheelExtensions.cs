693ce0e770ca514efb550fa903b08aafa6ad0898
Randy Skopecek <rskopecek@gmail.com>
2014-02-27 16:01:27 -0500

    Updated extension file names

---
+++ AzureDataEaseOfUse/Tables/Flywheel/FlywheelExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureDataEaseOfUse.Tables.Async
+{
+    public static class FlywheelExtensions
+    {
+        public static bool IsSuccessful(this TableResult result)
+        {
+            return (200 <= result.HttpStatusCode && result.HttpStatusCode < 300);
+        }
+
+        public static bool IsConflict(this TableResult result)
+        {
+            return (409 == result.HttpStatusCode);
+        }
+
+        public static bool IsSuccessful<T>(this Task<T> item)
+        {
+            return item.IsCompleted && !item.IsCanceled && !item.IsFaulted;
+        }
+
+        public static TableFlywheel<T> Flywheel<T>(this CloudTable table) where T : TableEntity, IAzureStorageTable
+        {
+            return new TableFlywheel<T>(table);
+        }
+
+    }
+}
--- AzureDataEaseOfUse/Tables/TableExtensions.cs
+++ AzureDataEaseOfUse/Tables/TableExtensions.cs
@@ -1,27 +1,186 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
+using AzureDataEaseOfUse.Tables;
 
 namespace AzureDataEaseOfUse.Tables.Async
 {
-    public static class FlywheelTableExtensions
+    public static class TableExtensions
     {
-        public static bool IsSuccessful(this TableResult result)
+
+        #region Add
+
+        public async static Task<TableResult> Add<T>(this CloudTable table, T item) where T : TableEntity, IAzureStorageTable
         {
-            return (200 <= result.HttpStatusCode && result.HttpStatusCode < 300);
+            item.SyncKeysOnRow();
+
+            //TODO: Add check to ensure partition & row key naming conforms to requirements
+
+            var operation = TableOperation.Insert(item);
+
+            var result = await table.ExecuteAsync(operation);
+
+            return result;
         }
 
-        public static bool IsConflict(this TableResult result)
+        #endregion
+
+        #region Retrieval
+
+        public async static Task<T> Get<T>(this CloudTable table, IAzureStorageTable item) where T : TableEntity, IAzureStorageTable
         {
-            return (409 == result.HttpStatusCode);
+            return await table.Get<T>(item.GetTableKeys());
         }
 
-        public static bool IsSuccessful<T>(this Task<T> item)
+        public async static Task<T> Get<T>(this CloudTable table, string partitionKey, string rowKey) where T : TableEntity, IAzureStorageTable
         {
-            return item.IsCompleted && !item.IsCanceled && !item.IsFaulted;
+            var keys = new TableKeys(partitionKey, rowKey);
+
+            return await table.Get<T>(keys);
+        }
+
+        public async static Task<T> Get<T>(this CloudTable table, TableKeys keys) where T : TableEntity, IAzureStorageTable
+        {
+            var operation = TableOperation.Retrieve<T>(keys.PartitionKey, keys.RowKey);
+
+            var value = await table.ExecuteAsync(operation);
+
+            var result = (T)value.Result;
+
+            return result;
+        }
+
+        //public async static Task<List<T>> Get<T>(this CloudTable table, string partitionKey, params string[] rowKeys) where T : TableEntity, IAzureStorageTable
+        //{
+        //    var results = new List<T>();
+
+        //    var batch = new TableBatchOperation();
+            
+        //    foreach (var rowKey in rowKeys)
+        //    {
+        //        batch.Retrieve<T>(partitionKey, rowKey);
+
+
+        //        if (batch.IsFull())
+        //        {
+        //            var items = await table.ExecuteBatchAsync(batch);
+
+        //            foreach (var item in items)
+        //            {
+        //                if (item.IsSuccessful())
+        //                    results.Add((T)item.Result);                    
+        //            }
+                    
+        //            batch = new TableBatchOperation();
+        //        }
+        //    }
+
+        //    return results;
+        //}
+
+        public static bool IsFull(this TableBatchOperation batch)
+        {
+            return batch.Count == 100;
+        }
+
+
+        /// <summary>
+        /// [Synchronous execution]
+        /// </summary>
+        public static List<T> List<T>(this CloudTable table, string partitionKey) where T : TableEntity, IAzureStorageTable, new()
+        {
+            var results = table.Where<T>(q => q.PartitionKey == partitionKey).ToList();
+
+            return results;
+        }
+
+        /// <summary>
+        /// [Synchronous execution]
+        /// </summary>
+        public static List<T> Where<T>(this CloudTable table, Expression<Func<T, bool>> predicate) where T : TableEntity, IAzureStorageTable, new()
+        {
+            return table.CreateQuery<T>().Where(predicate).ToList();
+        }
+
+        #endregion
+
+        #region Update
+
+        public async static Task<TableResult> Update<T>(this CloudTable table, T item) where T : TableEntity, IAzureStorageTable
+        {
+            item.SyncKeysOnRow();
+            
+            var operation = TableOperation.Replace(item);
+
+            var result = await table.ExecuteAsync(operation);
+
+            return result;
+        }
+
+        #endregion
+
+        #region Deletion
+
+        public async static Task<TableResult> Delete<T>(this CloudTable table, T item) where T : TableEntity, IAzureStorageTable
+        {
+            item.SyncKeysOnRow();
+
+            var operation = TableOperation.Delete(item);
+
+            var result = await table.ExecuteAsync(operation);
+
+            return result;
+        }
+
+        public async static Task<bool> Delete(this CloudTable table, bool confirm)
+        {
+            return confirm ? await table.DeleteIfExistsAsync() : false;
+        }
+
+        #endregion
+
+        #region Table(s)
+
+        /// <summary>
+        /// [Synchronous execution]
+        /// </summary>
+        public static IEnumerable<CloudTable> Tables(this CloudStorageAccount account, string prefix = null)
+        {
+            var client = account.CreateCloudTableClient();
+            
+            var tables = client.ListTables(prefix);
+
+            return tables;
+        }
+
+
+        public async static Task<CloudTable> Table(this CloudStorageAccount account, string name, bool createIfNotExists = true)
+        {
+            //TODO: Add check to ensure table name conforms to requirements
+
+            var client = account.CreateCloudTableClient();
+
+            var table = client.GetTableReference(name);
+            
+            if (createIfNotExists)
+                await table.CreateIfNotExistsAsync();
+
+            return table;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Ensures the PartitionKey and Row Keys are set on item
+        /// </summary>
+        public static void SyncKeysOnRow<T>(this T item) where T : TableEntity, IAzureStorageTable
+        {
+            item.GetTableKeys().SyncTo(item);        
         }
 
     }
